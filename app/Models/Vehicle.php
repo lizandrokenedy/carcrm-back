8fc0f8650b9f066178fdd48f292e67fe6621d156
Lizandro Kenedy <lizandro.santos@grupolpj.com>
2021-04-24 18:31:30 -0400

    correçao veiculo apagado

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -22,6 +22,20 @@ class Vehicle extends Model
         'vehicle_financial' => Json::class,
     ];
 
+    static $rules = [
+        'zipCode' => 'required',
+        'city' => 'required',
+        'uf' => 'required',
+        'vehicle_type' => 'required',
+        'vehicle_brand' => 'required',
+        'vehicle_model' => 'required',
+        'vehicle_version' => 'required',
+        'vehicle_regdate' => 'required',
+        'vehicle_fuel' => 'required',
+        'vehicle_price' => 'required',
+        'vehicle_photos' => 'exists:vehicle_photos,vehicle_id',
+    ];
+
     protected $fillable = [
         "user_id",
         "tag_id",
